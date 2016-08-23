1810749e14a02669ba5ae2ad9e8f03ee2b82dd15
shogogg <shogo@studiofly.net>
2016-08-23 15:44:34 +0900

    :bug: 最新の homebrew-cask で動作しない問題を解消

--- Casks/atok-passport.rb
+++ Casks/atok-passport.rb
@@ -1,8 +1,14 @@
-class AtokPassport < Cask
-  url 'https://gate.justsystems.com/download/atok/ut/mac/at29try.dmg'
-  homepage 'http://atok.com/'
+cask 'atok-passport' do
   version '2016'
   sha256 'bda513644013a35c352624f74b4b918f35650e9a88155ecc758d978cddb1a9ae'
-  install 'ATOK 2016 インストーラ.pkg'
-  uninstall :script => '/Applications/JustSystems/ATOK 2016/ATOK Uninstaller.app/Contents/MacOS/ATOK Uninstaller'
+  
+  url 'https://gate.justsystems.com/download/atok/ut/mac/at29try.dmg'
+  name 'ATOK 2016'
+  homepage 'http://atok.com/'
+  license :commercial
+  
+  pkg "ATOK #{version} インストーラ.pkg"
+  
+  uninstall :script => "/Applications/JustSystems/ATOK #{version}/ATOK Uninstaller.app/Contents/MacOS/ATOK Uninstaller"
+
 end
