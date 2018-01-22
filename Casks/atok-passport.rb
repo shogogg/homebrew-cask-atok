a9e5cf80df74b1fa04d8b4930eccbedaf521df46
keitarou <keitarou.oonishi@gmail.com>
2018-01-22 20:53:50 +0900

    fixed sha256

--- Casks/atok-passport.rb
+++ Casks/atok-passport.rb
@@ -1,6 +1,6 @@
 cask 'atok-passport' do
   version '2017'
-  sha256 '93F4DBA27D1C2CD3CCB48EF1566BF337B3CED33197E5CBEA795DC3FEE28241E4'
+  sha256 'f5a292543e2b15bd905d654bd5623b154ec311da071033556a03da3574971b83'
   
   url 'https://gate.justsystems.com/download/atok/ut/mac/at30try.dmg'
   name 'ATOK 2017'
